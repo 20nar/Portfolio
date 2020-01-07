b9e153445663df9f08aed2d0152c9e0bf6ccdb04
Zonar Caldera <zonar_94@hotmail.com>
2020-01-07 12:45:55 +0100

    Animations

---
+++ Portfolio V4/root/js/scrolldown.js
@@ -0,0 +1,21 @@
+
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
+
+$(function(){
+      SyntaxHighlighter.all();
+    });
+    $(window).load(function(){
+      $('.flexslider').flexslider({
+        animation: "slide",
+        start: function(slider){
+          $('body').removeClass('loading');
+        }
+      });
+    });
+$('#yourElement').addClass('animated bounceOutLeft');
+new WOW().init();
